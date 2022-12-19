163f214d83173082a9a12ca8634b318ddf5b8b77
kero-cl <matias.quero@gmail.com>
2022-12-19 11:35:10 +0200

    trying to deploy form

---
+++ users/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Hours
+
+
+
+class UserWorkForm(forms.ModelForm):
+   
+    class Meta:
+        model = Hours
+        fields = ['start', 'end']
--- users/views.py
+++ users/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.views.generic import ListView, CreateView
-from .models import Profile
+from .models import Profile, Hours
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .forms import UserWorkForm
+from django.contrib import messages
 
 @login_required
 
 def home(request):
-    
+    if request.method == 'POST':
+        form = UserWorkForm(request.POST)
+
+        if form.is_valid():
+            form.save()            
+            messages.success(request, f'Your worked has finished')
+            return redirect('profile')
+
     return render(request, 'users/home.html')
 
 
@@ -16,19 +25,20 @@ def home(request):
 
 
 #class HoursCreateView(LoginRequiredMixin, CreateView):
-#    model = Profile
-#    fields = ['hours', 'minutes']
+#    model = Hours
+#    fields = ['start', 'end']
+#
 #    template_name = 'users/home.html'
-
+#
 #    def form_valid(self, form):
-#        form.instance.author = self.request.user
+#        form.instance.worker = self.request.user
 #        return super().form_valid(form)
 
 @login_required
 
 def profile(request):
-     
-    return render(request, 'users/profile.html')
+
+        return render(request, 'users/profile.html')
 
 
 
